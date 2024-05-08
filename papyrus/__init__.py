e85b3da7c9eb28d9df007143c9caa883ca0f1fed
knikolaou <knikolaou@icp.uni-stuttgart.de>
2024-05-08 15:29:32 +0200

    Remove requirements from the init file

--- papyrus/__init__.py
+++ papyrus/__init__.py
@@ -1,14 +1,8 @@
-[build-system]
-requires = ["setuptools", "setuptools-scm"]
-build-backend = "setuptools.build_meta"
+"""
+Papyrus
+=======
+Papyrus is a simple, lightweight Python library for recording neural learning. 
 
-[project]
-name = "my_package"
-authors = [
-    {name = "Example Author", email = "author@example.com"}
-]
-description = "package description"
-version = "0.0.1"
-readme = "README.md"
-requires-python = ">=3.10"
-dependencies = {file = ["requirements.txt"]}
+Summary
+-------
+"""
