c445c4ac86a5292653a262a348855d80c8b0b8bd
knikolaou <knikolaou@icp.uni-stuttgart.de>
2024-05-08 18:34:50 +0200

    Include header in existing files.

--- CI/unit_tests/test_dummy_test.py
+++ CI/unit_tests/test_dummy_test.py
@@ -1,7 +1,25 @@
 """
-Some Information about this module
-"""
+papyrus: a lightweight Python library to record neural learning.
 
+License
+-------
+This program and the accompanying materials are made available under the terms
+of the Eclipse Public License v2.0 which accompanies this distribution, and is
+available at https://www.eclipse.org/legal/epl-v20.html
+
+SPDX-License-Identifier: EPL-2.0
+
+Copyright Contributors to the Zincwarecode Project.
+
+Contact Information
+-------------------
+email: zincwarecode@gmail.com
+github: https://github.com/zincware
+web: https://zincwarecode.com/
+
+Summary
+-------
+"""
 
 class TestDummyTest:
     """
